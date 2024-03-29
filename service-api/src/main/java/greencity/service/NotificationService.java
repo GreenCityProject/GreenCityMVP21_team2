68ed590d7f6a8e0e797566d349ee8502590ac17d
ryhaldenys <denys.ryhal.knm.2020@lpnu.ua>
2024-03-29 15:22:43 +0200

    [224] improved notification end-points

--- service-api/src/main/java/greencity/service/NotificationService.java
+++ service-api/src/main/java/greencity/service/NotificationService.java
@@ -1,21 +1,23 @@
 package greencity.service;
 
-import greencity.dto.econews.EcoNewsDto;
+import greencity.dto.CreatNotificationDto;
 import greencity.dto.notification.NotificationDto;
 import greencity.dto.user.UserVO;
 
 import java.util.List;
 
 public interface NotificationService {
-    List<NotificationDto> getAllNotifications(String userEmail, String lang);
+    List<NotificationDto> getAllNotifications(Long useId);
 
-    List<NotificationDto> getLatestUnreadNotifications(String userEmail, String lang);
+    List<NotificationDto> getLatestUnreadNotifications(Long userId);
 
     void removeNotificationById(UserVO user, Long id);
 
-    NotificationDto changeStatusToRead(Long id, UserVO user, String lang);
+    NotificationDto changeStatusToRead(Long id, UserVO user);
 
-    NotificationDto changeStatusToUnread(Long id, UserVO user, String lang);
+    NotificationDto changeStatusToUnread(Long id, UserVO user);
 
-    void createNotification (List<String> text, UserVO userVO, Long autorId, String title);
+    //void createNotification (List<String> text, UserVO userVO, Long autorId, String title);
+
+    void createNotification (CreatNotificationDto creatNotificationDto);
 }
