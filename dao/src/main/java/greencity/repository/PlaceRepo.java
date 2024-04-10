e528edd8775b6e25041385bbcdded5d869e7e346
Vasyl Shtoiko <vasylshtoiko@Vasyls-MacBook-Pro.local>
2024-04-10 22:49:25 +0200

    Add findByFavoritesId() method to PlaceRepo

--- dao/src/main/java/greencity/repository/PlaceRepo.java
+++ dao/src/main/java/greencity/repository/PlaceRepo.java
@@ -5,7 +5,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface PlaceRepo extends JpaRepository<Place, Long>, JpaSpecificationExecutor<Place> {
-
+    List<Place> findByFavoritesId(Long userId);
 }
